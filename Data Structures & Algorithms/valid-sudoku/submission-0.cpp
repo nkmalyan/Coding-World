dfeff30c06d056b1d2c396de5218c0f85e609789
nkmalyan <nitinkumarmalyan.010@gmail.com>
2026-08-16 14:58:05 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board){
+
+        bool rows[9][9] = {false};
+        bool cols[9][9] = {false};
+        bool boxes[9][9] = {false};
+        
+        for (int r = 0; r < 9; r++){
+            for (int c = 0; c < 9; c++){
+                
+                if (board[r][c] == '.'){
+                    continue;
+                }
+                
+                int num = board[r][c] - '1';
+                int k = (r / 3) * 3 + (c / 3);
+
+                if (rows[r][num] || cols[c][num] || boxes[k][num]){
+                    return false;
+                }
+                rows[r][num] = true;
+                cols[c][num] = true;
+                boxes[k][num] = true;
+            }
+        }
+        return true;
+    }
+};
